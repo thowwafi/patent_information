ee5e08e257db7660c0ee344fdefdd048161228d7
alfiansyah@baezeni.com <alfiansyah@baezeni.com>
2022-01-26 16:54:09 +0700

    add conver to csv

---
+++ convert_to_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def convert_to_csv(input_path, output_path):
+    """
+    Convert all .xlsx files in input_path to .csv files in output_path.
+    """
+    df = pd.read_excel(input_path, dtype=str)
+    print(df.shape)
+    df.to_csv(output_path, index=False, sep=";")
+
+
+if __name__ == '__main__':
+    input_path = 'patents_2000_2021_new.xlsx'
+    output_path = 'patents_2000_2021.csv'
+    convert_to_csv(input_path, output_path)
+    print('Done!')
