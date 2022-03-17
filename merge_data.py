cc3b304b485bd98b0626b5e6d9730b885e052baa
alfiansyah@baezeni.com <alfiansyah@baezeni.com>
2022-03-17 12:05:00 +0700

    update csv

--- merge_data.py
+++ merge_data.py
@@ -7,21 +7,22 @@ import pandas as pd
 
 home = os.getcwd()
 OUTPUT_PATH = os.path.join(home, 'new_output')
+MERGED_PATH = 'patents_2010_2021'
 
 if __name__ == '__main__':
     appended_data = []
     files = glob.glob(f"{OUTPUT_PATH}/*.xlsx")
     files = sorted(files)
-    for infile in files[20:]:
+    for infile in files[30:]:
         print(infile)
         data = pd.read_excel(infile, dtype=str)
+        infile_csv = infile.replace('.xlsx', '.csv').replace('new_output', 'output_csv')
+        data.to_csv(infile_csv, index=False, sep=";", encoding="utf-8-sig")
         appended_data.append(data)
 
-    appended_data = pd.concat(appended_data)
-    # appended_data['country_code'] = appended_data.applicant_address.str.split(' ').str[-1].str.strip()
-    # column_to_move = appended_data.pop("country_code")  
-    # appended_data.insert(2, "country_code", column_to_move)
-    print(appended_data.columns)
-    appended_data.to_excel('patents_1980_1999_new.xlsx', index=False)
-    # appended_data.to_csv('patents_2000_2021_new.csv', index=False, sep=";", encoding="utf-8-sig")
+    all_data = pd.concat(appended_data)
+    print(all_data.columns)
+    all_data.to_excel(f'{MERGED_PATH}.xlsx', index=False)
+    print("Excel Done!")
+    all_data.to_csv(f'{MERGED_PATH}.csv', index=False, sep=";", encoding="utf-8-sig")
     print('Done!')
