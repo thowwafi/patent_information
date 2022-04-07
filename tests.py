1d5a019cb4a240e76abd930f3e97cf5151fccb09
Thowwafi <thowwafi@gmail.com>
2022-04-07 12:18:09 +0700

    add no sunbox

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = ChromeOptions()
+options.add_argument("no-sandbox")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--start-maximized")
 options.add_argument("--headless")
