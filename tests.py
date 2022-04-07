3a3123553f191a8cce4b9b7acc9159bd63ec68ca
Thowwafi <thowwafi@gmail.com>
2022-04-07 11:57:12 +0700

    add test py

---
+++ tests.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = ChromeOptions()
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--start-maximized")
+options.add_argument("--headless")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://www.google.com")
+print(driver.title)
